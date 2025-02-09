8f454589577ad7687902626f9635f3647840a54f
sanjeev <kumarsanjeevdutta02@gmail.com>
2025-02-09 13:25:30 +0530

    python codes

---
+++ Conditionals/01_solution.py
@@ -0,0 +1,23 @@
+
+#  problem 02 moview ticket
+
+age = 9
+day = "wednesday"
+
+price = 12 if age >= 18 else 8
+
+if day == "wednesday":
+    price = price - 2
+
+# print(price)
+
+weather = "Sunny"
+
+if weather == "Sunny":
+    activity = "Go for a walk"
+elif weather == "Rainy":
+    activity = "Read a book"
+elif weather == "Snowy":
+    activity = "Build a snowman"
+
+print(activity)
