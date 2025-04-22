b2f848235db83b8866f06af08cd39e1e352b54e7
sanjeev <kumarsanjeevdutta02@gmail.com>
2025-04-22 13:18:18 +0530

    new code added

---
+++ functions/solution.py
@@ -0,0 +1,25 @@
+# 3. Polymorphism in Functions
+def multiply (p1,p2):
+    return p1 * p2
+
+
+print(multiply(2,3))
+print(multiply(2,'3'))
+print(multiply('a',3))
+
+# default value function
+
+def greet(name="User"):
+    return 'Hello'+ " " + name
+
+
+print(greet("Anil"))
+print(greet())
+
+# lambda function
+
+cube = lambda x: x ** 3
+
+print(cube(3))
+
+args
